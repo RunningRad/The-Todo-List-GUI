4412536f4b3d947461b0b6e994b67d35cc2b109c
Lance <SoccerLanceTobey@gmail.com>
2025-06-03 14:44:53 -0600

    Update addGUI.py
    Created a basic change to the addGUI page that allows it to take text input. It currently returns it.

--- addGUI.py
+++ addGUI.py
@@ -1,1 +1,20 @@
-print("Added")
+from tkinter import *
+
+gui = Tk()
+
+gui.title("Add Item To To-Do List")
+gui.geometry('540x240')
+
+text = Label(gui, text = "Put what item you want to add to the To-Do list here.")
+text.grid()
+
+entryItem = Entry(gui, width = 40)
+entryItem.grid()
+
+def addItemButton():
+    print(entryItem.get())
+
+addButton = Button(gui, text="Add to list", command=addItemButton)
+addButton.grid()
+
+gui.mainloop()
