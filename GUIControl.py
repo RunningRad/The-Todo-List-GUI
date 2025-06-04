2886dd07e4012f4c1c3695a45ee7a295dd85f681
Lance <SoccerLanceTobey@gmail.com>
2025-06-04 14:08:00 -0600

    Comments
    Added comments to the existing code.

--- GUIControl.py
+++ GUIControl.py
@@ -1,24 +1,30 @@
 from tkinter import *
 import subprocess
 
+# Runs the addGUI.py page.
 def addButton():
     subprocess.run(["python", "addGUI.py"])
 
+# Runs the removeGUI.py page.
 def removeButton():
     subprocess.run(["python", "removeGUI.py"])
 
+# Initializes the GUI
 gui = Tk()
 
+# Basic code that gives the GUI it's structure.
 gui.title("To-Do List Organizer")
 gui.geometry('360x240')
 
 #introText = Label(gui, text = "Do you want to add or remove from the list?")
 #introText.grid()
 
+# Creates the buttons for the GUI
 addButton = Button(gui, text = "Add", width = 24, command = addButton)
 removeButton = Button(gui, text = "Remove", width = 24, command= removeButton )
 
 addButton.grid(row=1,column=0)
 removeButton.grid(row=1,column=1)
 
+# Runs the GUI
 gui.mainloop()
--- addGUI.py
+++ addGUI.py
@@ -1,22 +1,25 @@
 from tkinter import *
 import os.path
 
-
+# Creates an index to show how many items are on the list.
 def setIndex():
     global index
     index = 1
 
 setIndex()
 
+# Makes sure that a todolist.txt file already exists. If it doesn't, it creates the file.
 if os.path.isfile("todolist.txt") is False:
     open("todolist.txt", 'x')
 
+# Goes through the items in the ToDo list and increments the index for each item added.
 with open("todolist.txt", 'r') as f:
     global index
     for line in f:
         if line.strip() != "":
             index += 1
 
+# Creates GUI skeleton.
 gui = Tk()
 
 gui.title("Add Item To To-Do List")
@@ -28,10 +31,15 @@ text.grid()
 entryItem = Entry(gui, width = 40)
 entryItem.grid()
 
+# Creates the add button functionality.
 def addItemButton():
     global index
+
+    # Takes the item and clears the text entry.
     text = entryItem.get()
     entryItem.delete(0,END)
+
+    # Adds the text entry to the todo list.
     with open("todolist.txt", "a") as f:
         f.write(str(index) + ". " + text + "\n\n")
         index += 1
