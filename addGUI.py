1696dc983a90e0b92355653c4cc8456a07b7b4bc
Lance <SoccerLanceTobey@gmail.com>
2025-06-04 12:10:03 -0600

    Fleshed out AddGUI.py
    Created the functionality of addGUI.py, it now creates a file and adds items the user gives it to the file.

--- addGUI.py
+++ addGUI.py
@@ -1,4 +1,21 @@
 from tkinter import *
+import os.path
+
+
+def setIndex():
+    global index
+    index = 1
+
+setIndex()
+
+if os.path.isfile("todolist.txt") is False:
+    open("todolist.txt", 'x')
+
+with open("todolist.txt", 'r') as f:
+    global index
+    for line in f:
+        if line.strip() != "":
+            index += 1
 
 gui = Tk()
 
@@ -12,7 +29,12 @@ entryItem = Entry(gui, width = 40)
 entryItem.grid()
 
 def addItemButton():
-    print(entryItem.get())
+    global index
+    text = entryItem.get()
+    entryItem.delete(0,END)
+    with open("todolist.txt", "a") as f:
+        f.write(str(index) + ". " + text + "\n\n")
+        index += 1
 
 addButton = Button(gui, text="Add to list", command=addItemButton)
 addButton.grid()
